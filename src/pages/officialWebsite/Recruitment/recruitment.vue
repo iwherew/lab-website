c86b6eb59921bcd69635b2f9f0457b6ca49f4777
iwhere <iwhere_w@126.com>
2020-03-08 14:09:34 +0800

    招生

--- src/pages/officialWebsite/Recruitment/recruitment.vue
+++ src/pages/officialWebsite/Recruitment/recruitment.vue
@@ -1,9 +1,23 @@
 <template>
-  <div class="">
+  <div class="recruitment">
+    <banner/>
+    <article-lists :type="recruitment"/>
   </div>
 </template>
 <script>
+  import banner from '../../../components/banner'
+  import articleLists from "../common/articleLists";
+  import recruitmentImage from '../../../images/officialWebsite/recruitment/recruitment.jpg'
   export default {
+    components: {banner,articleLists},
+    data(){
+      return{
+        recruitment:{
+          title: '招生信息',
+          imageUrl: recruitmentImage,
+        }
+      }
+    }
   }
 </script>
 <style scoped lang="less">
